137f77e3ea2ff59f29187b05bd409fb46577b3d6
Mihailova Tatiana <tatiana.mihailova@isa.utm.md>
2025-06-23 21:42:57 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-green-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-700">Tailwind работает! 🎉</h1>
+    </div>
+  );
+}
+
